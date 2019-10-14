0442a3b7e7da95b54a25f609be7ca0c9f145c683
radekczdev <radekcz.dev@gmail.com>
2019-10-14 20:46:36 +0200

    FindOdd modified

--- src/main/java/com/test/testapp/katas/FindOdd.java
+++ src/main/java/com/test/testapp/katas/FindOdd.java
@@ -5,12 +5,9 @@ import java.util.stream.Collectors;
 
 public class FindOdd {
     public static int findIt(int[] a) {
-        List<Integer> numbers = Arrays.stream(a).boxed().sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+        List<Integer> numbers = Arrays.stream(a).boxed().collect(Collectors.toList());
         for(int dist : a) {
-
-            List<Integer> toBeRemoved = new ArrayList<>();
-            toBeRemoved.add(dist);
-            numbers.removeAll(toBeRemoved);
+            numbers = numbers.stream().filter(element -> element != dist).collect(Collectors.toList());
             if(numbers.size() % 2 == 0)
                 return dist;
         }
