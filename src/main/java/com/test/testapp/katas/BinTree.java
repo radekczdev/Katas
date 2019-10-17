1c69bdadbd5b4d8ea1ab1245f019db8d38da126b
radekczdev <radekcz.dev@gmail.com>
2019-10-17 15:22:38 +0200

    BinTree almost done

--- src/main/java/com/test/testapp/katas/BinTree.java
+++ src/main/java/com/test/testapp/katas/BinTree.java
@@ -1,34 +1,71 @@
 package com.test.testapp.katas;
 
-public class BinTree {
+import sun.reflect.generics.tree.Tree;
 
+public class BinTree {
+    public static int[] values;
+    public static String lastPath = "right";
     static TreeNode arrayToTree(int[] array) {
-        System.out.println("Curr array: ");
         if(array.length == 0) {
             return null;
         }
-
-        TreeNode root = new TreeNode(array[0]);
-        System.out.print(array[0] + " ");
-        for (int i = 1; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-            root = addRecursively(root, array[i], array[i-1]);
-        }
-        System.out.println("END");
+        values = array;
+        int i = 0;
+        TreeNode root = new TreeNode(values[0]);
+        root = add(root, i);
+//        if(array.length == 0) {
+//            return null;
+//        }
+//        if(array.length == 1) {
+//            return new TreeNode(array[0]);
+//        }
+//        TreeNode root = new TreeNode(array[0]);
+//        for(int i=0; i < array.length; i++) {
+//            if(i > array.length-2*i) {
+//                root = addToNode(array[i], null, null, root);
+//            } else if(i > array.length-2*i+1) {
+//                root = addToNode(array[i], new TreeNode(array[2*i+1]), null, root);
+//            } else {
+//                root = addToNode(array[i], new TreeNode(array[2*i+1]), new TreeNode(array[2*i+2]), root);
+//            }
+//        }
         return root;
     }
 
-    static TreeNode addRecursively(TreeNode node, int value, int prevVal) {
-
-        if (node == null) {
-            return new TreeNode(value);
+    static TreeNode add(TreeNode node, int i) {
+        if(values.length < i) {
+            return null;
         }
+//        if(i > values.length - 2*i) {
+//            node = new TreeNode(values[i], add(node, 2*i + 1), add(node, 2*i + 2));
+//        } else if (i > values.length - 2*i + 1) {
+//            node = new TreeNode(values[i], add(node, 2*i + 1), add(node, 2*i + 2));
+//        } else {
+            node = new TreeNode(values[i], add(node, 2*i + 1), add(node, 2*i + 2));
+//        }
 
-        if(node.left == null || (node.right != null && (node.left.left == null || node.left.right == null))) {
-            node.left = addRecursively(node.left, value, prevVal);
-        } else if(node.right == null || (node.left != null && (node.right.left == null || node.right.right == null))) {
-            node.right = addRecursively(node.right, value, prevVal);
-        }
         return node;
     }
+
+//    static TreeNode addToNode(int value, TreeNode left, TreeNode right, TreeNode node) {
+//        boolean used = false;
+//        if(node == null) {
+//            return new TreeNode(value);
+//        }
+//        if(node.left == null) {
+//            node.left = left;
+//        }
+//        else if(node.right.toString().contains(" "+value+" ")){
+//            node.left = addToNode(value, left, right, node.left);
+//            used = true;
+////            lastPath = "left";
+//        }
+//        if(node.right == null) {
+//            node.right = right;
+//        } else if(!used) {
+//            node.right = addToNode(value, left, right, node.right);
+////            lastPath = "right";
+//        }
+//        return node;
+//    }
 }
--- src/test/java/com/test/testapp/katas/BinTreeTest.java
+++ src/test/java/com/test/testapp/katas/BinTreeTest.java
@@ -14,10 +14,23 @@ public class BinTreeTest {
 
     @Test
     public void arrayWithMultipleElements() {
-        TreeNode expected = new TreeNode(17, new TreeNode(0, new TreeNode(3), new TreeNode(15)), new TreeNode(-4));
-        assertThat(BinTree.arrayToTree(arrayFrom(17, 0, -4, 3, 15)), is(expected));
-        TreeNode expected2 = new TreeNode(17, new TreeNode(0, new TreeNode(3), new TreeNode(15)), new TreeNode(-4));
-        assertThat(BinTree.arrayToTree(arrayFrom(17, 0, -4, 3, 15)), is(expected2));
+//        TreeNode expected = new TreeNode(17, new TreeNode(0, new TreeNode(3), new TreeNode(15)), new TreeNode(-4));
+//        assertThat(BinTree.arrayToTree(arrayFrom(17, 0, -4, 3, 15)), is(expected));
+        TreeNode expected2 = new TreeNode(731,
+                new TreeNode(-306,
+                        new TreeNode(415,
+                                new TreeNode(-309),
+                                new TreeNode(-368)),
+                        new TreeNode(146,
+                                new TreeNode(955),
+                                new TreeNode(-380))),
+                new TreeNode(-490,
+                        new TreeNode(-150,
+                                new TreeNode(-785),
+                                new TreeNode(945)),
+                        new TreeNode(-846))
+        );
+        assertThat(BinTree.arrayToTree(arrayFrom(731, -306, -490, 415, 146, -150, -846, -309, -368, 955, -380, -785, 945)), is(expected2));
     }
 
     private int[] arrayFrom(int... values) {
